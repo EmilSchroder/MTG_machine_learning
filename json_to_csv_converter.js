d842f34395e32134a629f29bb9964fd65df7eacb
EmilSchroder <eschro66@gmail.com>
2019-02-18 08:44:03 +1300

    Switching to JS to process data

---
+++ json_to_csv_converter.js
@@ -0,0 +1,14 @@
+//Need to manipulate json data before writing to csv
+
+let data = [{'cardName': 'Flim',
+         'manaCost': '{2}{B}{B}',
+         'CMC': 4,
+         'type': 'Creature',
+         'rarity': 'Rare',
+         'power': 4,
+         'toughness': 5}]
+
+
+let processing = data.map(obj => {
+    obj
+})
