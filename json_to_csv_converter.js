2f3328a7f441fd67e210b7ef5398c407032424d2
EmilSchroder <eschro66@gmail.com>
2019-02-18 09:23:07 +1300

    figured out format of data processing

--- json_to_csv_converter.js
+++ json_to_csv_converter.js
@@ -1,14 +1,43 @@
 //Need to manipulate json data before writing to csv
 
-let data = [{'cardName': 'Flim',
-         'manaCost': '{2}{B}{B}',
-         'CMC': 4,
-         'type': 'Creature',
-         'rarity': 'Rare',
-         'power': 4,
-         'toughness': 5}]
+let data = {
+    "\"Ach! Hans, Run!\"": {
+        "colorIdentity": [
+            "G",
+            "R"
+        ],
+        "colors": [
+            "G",
+            "R"
+        ],
+        "convertedManaCost": 6.0,
+        "foreignData": [],
+        "layout": "normal",
+        "legalities": {},
+        "manaCost": "{2}{R}{R}{G}{G}",
+        "name": "\"Ach! Hans, Run!\"",
+        "printings": [
+            "UNH"
+        ],
+        "rulings": [],
+        "scryfallId": "84f2c8f5-8e11-4639-b7de-00e4a2cbabee",
+        "subtypes": [],
+        "supertypes": [],
+        "tcgplayerProductId": 37816,
+        "tcgplayerPurchaseUrl": "https://mtgjson.com/links/85b366724beadefd",
+        "text": "At the beginning of your upkeep, you may say \"Ach Hans, run It\'s the . . .\" and the name of a creature card. If you do, search your library for a card with that name, put it onto the battlefield, then shuffle your library. That creature gains haste. Exile it at the beginning of the next end step.",
+        "type": "Enchantment",
+        "types": [
+            "Enchantment"
+        ],
+        "uuid": "d4492969-dc3a-5617-bc14-4f15afc12b2b"
+    }
+}
+let jsonKeys = Object.keys(data)
 
+let revisedData = []
 
-let processing = data.map(obj => {
-    obj
+jsonKeys.map(key => {
+
+    console.log(data[key]['type'])
 })
